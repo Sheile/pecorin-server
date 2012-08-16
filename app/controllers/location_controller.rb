dd8cffeca84b16e0da1d2697616e423ecd765b54
Sheile <sheile1024@gmail.com>
2012-08-17 07:33:16 +0900

    近くにいるPecori可能ユーザをHTTP Responseで返すように変更

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,6 +2,9 @@ class LocationController < ApplicationController
   def update
     user = User.where(:facebook_id => params[:facebook_id]).first || nil
     user.update_attribute(:current_location_id, params[:current_location_id])
-    render :json => {:messate => "ok"}
+
+    # 近くにいるpecori可能なユーザを返す
+    near_users = User.find_all_by_current_location_id(params[:current_location_id])
+    render :json => {:messate => "ok", :near_users = near_users}
   end
 end
